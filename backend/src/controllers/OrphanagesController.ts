8a9cbfb46264b7c24dbe879cef4fd9af1898802b
Isac X <isacssw@gmail.com>
2021-04-14 13:39:15 +0100

    images upload done

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -36,6 +36,12 @@ export default {
         } = req.body;
     
         const orphanageRepository = getRepository(Orphanage);
+
+        const requestImages = req.files as Express.Multer.File[];
+
+        const images = requestImages.map(image => {
+            return { path: image.filename}
+        })
     
         const orphanage = orphanageRepository.create({
             name,
@@ -45,6 +51,7 @@ export default {
             instructions,
             opening_hours,
             open_on_weekends,
+            images,
         });
     
         await orphanageRepository.save(orphanage);
