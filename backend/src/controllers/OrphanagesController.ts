9baa6b9a5e3b1ded4a4dfbd874c14097742ecdb5
Isac X <isacssw@gmail.com>
2021-04-14 12:40:54 +0100

    uploading images in our database

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -3,6 +3,27 @@ import { getRepository } from 'typeorm';
 import Orphanage from '../models/Orphanage';
 
 export default {
+
+    async index(req: Request, res: Response) {
+
+        const orphanageRepository = getRepository(Orphanage);
+
+        const orphanages = await orphanageRepository.find();
+
+        return res.json(orphanages);
+    },
+
+    async show(req: Request, res: Response) {
+
+        const { id } = req.params;
+
+        const orphanageRepository = getRepository(Orphanage);
+
+        const orphanage = await orphanageRepository.findOneOrFail(id);
+
+        return res.json(orphanage);
+    },
+
     async create(req: Request, res: Response): Promise<Response> {
         const {
             name,
