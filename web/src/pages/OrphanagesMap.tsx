f01992e6399b64dbc1b584c1719f3940ea874a1a
Isac X <isacssw@gmail.com>
2021-04-15 19:05:22 +0100

    adding marker and popup in map

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import Leaflet from 'leaflet';
 
 import 'leaflet/dist/leaflet.css';
 
 import mapMarkerImg from '../images/map-marker.svg';
 
 import '../styles/pages/orphanages-map.css';
+
+const mapIcon = Leaflet.icon({
+    iconUrl: mapMarkerImg,
+
+    iconSize: [58,68],
+
+    iconAnchor:[29,68],
+
+})
 
 function OrphanagesMap () {
     return (
@@ -32,6 +42,18 @@ function OrphanagesMap () {
                 style= {{ width: '100%', height: '100%'}}
             >
                 <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}/>
+
+                <Marker
+                    icon = {mapIcon}
+                    position={[38.7913255,-9.3340266]}
+                >
+
+                    <Popup closeButton={false} minWidth={240} maxWidth={240}>
+                        Lar das meninas
+                    </Popup>
+
+                </Marker>
+
             </MapContainer>
 
             <Link to="" className="create-orphanage">
